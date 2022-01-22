171d44695fce014bfffd1f7e4c97c8c173d7fd53
kenedfelix <kened.felix@gmail.com>
2022-01-22 16:52:16 -0300

    Criado Metodo Alterar Pessoa

--- src/main/java/br/com/apipessoas/resource/PessoaController.java
+++ src/main/java/br/com/apipessoas/resource/PessoaController.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +37,26 @@ public class PessoaController {
 	@GetMapping("/listarPessoa/{id}")
 	public Optional<Pessoa> getPessoa(@PathVariable int id){
 		return repository.findById(id);
+	}
+	
+	@PutMapping("/editarPessoa/{id}")
+	public ResponseEntity<Pessoa> getPessoaEdit(@PathVariable int id, @RequestBody Pessoa pessoa){
+		 Optional<Pessoa> pessoaData = repository.findById(id);
+		 
+		 if (pessoaData.isPresent()) {
+		 Pessoa _pessoa = pessoaData.get();
+		 _pessoa.setNome(pessoa.getNome());
+		 _pessoa.setSexo(pessoa.getSexo());
+		 _pessoa.setEmail(pessoa.getEmail());
+		 _pessoa.setData_nascimento(pessoa.getData_nascimento());
+		 _pessoa.setNaturalidade(pessoa.getNaturalidade());
+		 _pessoa.setNacionalidade(pessoa.getNacionalidade());
+		 _pessoa.setCpf(pessoa.getCpf());
+		 return new ResponseEntity<Pessoa>(repository.save(_pessoa), HttpStatus.OK);
+		 }
+		 else {
+			    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 	
 	@DeleteMapping("/deletarPessoa/{id}")
